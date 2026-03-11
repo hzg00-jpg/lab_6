20f2b0e0362bee9b9a4443761c4487b7ee95314f
hzg00-jpg <zhaoga79@gmail.com>
2026-03-11 16:01:16 -0700

    bug fixes

--- temperature.sh
+++ temperature.sh
@@ -5,10 +5,10 @@ read temp
 unit=${temp: -1}
 num=${temp:0:-1}
  
-if [[ $unit == "C" ]] then
+if [[ $unit == "C" ]] ; then
     answer=$(echo "scale=2; ($num * 9/5) + 32" | bc)
     echo "temp = ${answer}F"
-elif [[ $unit == "F" ]] then
+elif [[ $unit == "F" ]] ; then
     answer=$(echo "scale=2; ($num - 32) * 5/9" | bc)
     echo "$temp = ${answer}C"
 fi
